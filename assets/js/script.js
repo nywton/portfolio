cd338868a377a4702df767a4abecf66356eed4c7
Nywton Barros <nywton.barros@gmail.com>
2024-09-30 06:59:53 -0300

    Dark mode fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.main__img img');
+  const images = document.querySelectorAll('.main__img svg');
   let currentIndex = 0;
 
-  // Function to show current image and hide others
   function showImage(index) {
     images.forEach((img, i) => {
       img.classList.toggle('hidden', i !== index);
     });
   }
 
-  // Initially show the first image
   showImage(currentIndex);
 
-  // Set interval to change image every 1 second
   setInterval(() => {
     currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
-  }, 1000);
+  }, 1500);
 });
 
 function dark() {
