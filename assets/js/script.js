aba2c530c8cd6ebf411211a91930a7d41ebf2dc6
Nywton Barros <nywton.barros@gmail.com>
2024-10-01 14:56:52 -0300

    adding animations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.start_carousell svg');
+  animateLoading();
+  animateStartIcons();
+});
+
+function dark() {
+  document.body.classList.toggle("dark");
+  toggleVisibility();
+}
+
+function toggleVisibility() {
+  const btnLight = document.getElementById('btn-switch-light');
+  const btnDark = document.getElementById('btn-switch-dark');
+
+  btnLight.classList.toggle('hidden');
+  btnDark.classList.toggle('hidden');
+}
+
+function animateLoading() {
+  gsap.fromTo(
+    ".loading",
+    {
+      opacity: 1
+    },
+    {
+      opacity: 0,
+      display: "none",
+      duration: .5,
+      delay: 3,
+    }
+  );
+
+  gsap.fromTo(
+    ".logo-name",
+    {
+      y: 50,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
+    }
+  );
+}
+
+function animateStartIcons() {
   let currentIndex = 0;
+
+  const images = document.querySelectorAll('.start_carousell svg');
 
   function showImage(index) {
     images.forEach((img, i) => {
@@ -14,18 +62,5 @@ document.addEventListener('DOMContentLoaded', () => {
     currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
   }, 1000);
-});
-
-function dark() {
-  document.body.classList.toggle("dark");
-  toggleVisibility();
 }
 
-function toggleVisibility() {
-  const btnLight = document.getElementById('btn-switch-light');
-  const btnDark = document.getElementById('btn-switch-dark');
-
-  // Toggle the 'hidden' class on both elements
-  btnLight.classList.toggle('hidden');
-  btnDark.classList.toggle('hidden');
-}
